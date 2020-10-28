6ff9a84f3885c860a31c500246afb8b207007002
dyushakolesov <dyushakolesov@mail.ru>
2020-10-28 15:49:50 +0300

    sign in form added

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=e-library.py
---
+++ app/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
+
+class RegestrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('RepeatPassword', validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('User alreaady exists.')
+    
+    def validate_email(self,email):
+        user = User.query.filter_by(username=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a diffirent email.')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
