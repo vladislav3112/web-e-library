5df71f8ef22bdc1adfbb56e03601df50a0baeb10
dyushakolesov <dyushakolesov@mail.ru>
2020-11-15 19:09:09 +0300

    heroku deployment changes (test)

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed, FileRequired
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
